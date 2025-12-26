726f0c7cea012ba9a8c62f710f7c25fc25130c29
Giacomo Montibeller <giacomo.montibeller@gmail.com>
2025-12-26 11:02:15 +0000

    list id is a uuid

--- internal/todo/list.go
+++ internal/todo/list.go
@@ -1,13 +1,15 @@
 package todo
 
+import "github.com/google/uuid"
+
 type List struct {
-	id    int
+	id    uuid.UUID
 	tasks []string
 }
 
 func New() List {
 	return List{
-		id:    1,
+		id:    uuid.New(),
 		tasks: []string{},
 	}
 }
--- internal/todo/list_test.go
+++ internal/todo/list_test.go
@@ -1,11 +1,16 @@
 package todo
 
-import "testing"
-import "github.com/stretchr/testify/assert"
+import (
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+)
 
 func CreateList(t *testing.T) {
 	list := New()
 
 	assert.NotNil(t, list)
+	assert.NotEqual(t, uuid.Nil, list.id)
 	assert.Len(t, list.tasks, 0)
 }
